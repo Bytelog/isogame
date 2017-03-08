787cc20294f34016be29edb024123bb56a525099
Dylan Allbee <dylan@allbee.org>
2017-03-07 22:22:40 -0800

    Grass on the screen

--- main.go
+++ main.go
@@ -3,19 +3,26 @@ package main
 import "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	raylib.InitWindow(800, 450, "raylib [core] example - basic window")
+	screenWidth := int32(1300)
+	screenHeight := int32(700)
+
+	raylib.InitWindow(screenWidth, screenHeight, "ISOGAME")
+	defer raylib.CloseWindow()
 
 	raylib.SetTargetFPS(60)
 
+	img := raylib.LoadImage("grass.png")
+	defer raylib.UnloadImage(img)
+
+	raylib.ImageResize(img, img.Width/2, img.Height/2)
+	tile := raylib.LoadTextureFromImage(img)
+	defer raylib.UnloadTexture(tile)
+
 	for !raylib.WindowShouldClose() {
 		raylib.BeginDrawing()
-
 		raylib.ClearBackground(raylib.RayWhite)
-
-		raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, raylib.LightGray)
-
+		raylib.DrawTexture(tile, screenWidth/2-tile.Width/2, screenHeight/2-tile.Height/2, raylib.White)
 		raylib.EndDrawing()
 	}
 
-	raylib.CloseWindow()
 }
